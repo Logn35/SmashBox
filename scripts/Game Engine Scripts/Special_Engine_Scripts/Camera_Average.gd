2f7f669892b1c325c37fcc28ea010b6e51bad707
Lognes <lognes.kek@gmail.com>
2020-04-19 22:53:29 +0100

    Added "Player" Object

--- scripts/Game Engine Scripts/Special_Engine_Scripts/Camera_Average.gd
+++ scripts/Game Engine Scripts/Special_Engine_Scripts/Camera_Average.gd
@@ -1,16 +1,16 @@
 extends Node
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#Average of Player positions
+#Reset the goal position
+func Camera_Average():
+	cam_x_goal = 0
+	cam_y_goal = 0
+	var _num_of_active_players = 0
+	var _set = true
+	var _player_max_x = 0
+	var _player_min_x = 0
+	var _player_max_y = 0
+	var _player_min_y = 0
+	
+	#Get player positions
+	
+	
