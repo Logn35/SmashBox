52ad8708d62d143c24206f6a4336638f017b07d1
HiddenRealm <blacktumble123@gmail.com>
2020-04-20 21:09:27 -0700

    smoother movement

--- scripts/KinematicBody2D.gd
+++ scripts/KinematicBody2D.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 const up = Vector2(0, -1)
+const acceleration = 50
 const gravity = 500
-const speed = 500
+const maxspeed = 500
 const jumpHeight = -10000
 
 var motion = Vector2()
@@ -11,11 +12,11 @@ func _physics_process(delta):
 	motion.y =+ gravity
 	
 	if Input.is_action_pressed("moveRight"):
-		motion.x = speed
+		motion.x = min(motion.x+acceleration, maxspeed)
 	elif Input.is_action_pressed("moveLeft"):
-		motion.x = -speed
+		motion.x = max(motion.x-acceleration, -maxspeed)
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
 		
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
