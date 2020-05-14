e6898dbde3d341950f97c3ec422635a6d1ba8311
HiddenRealm <blacktumble123@gmail.com>
2020-05-14 16:15:18 -0700

    camera movement changes- broken rn, detach the script from the camera to get it working again

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -1,6 +1,17 @@
 extends Camera2D
 
-onready var camera1 = $"Camera2D2"
+var object1 = NodePath("res://Actors/TestCharacter")
+var object2 = NodePath("res://Actors/TestCharacter")
 
 func _ready():
-	camera1.target = get_node("TestCharacter2")
+	object1 = get_node(object1)
+	object2 = get_node(object2)
+
+func _process(delta):
+	self.global_position = (object1.global_position + object2.global_position) * 0.5
+
+	var zoom_factor1 = abs(object1.global_position.x-object2.global_position.x)/(1920-100)
+	var zoom_factor2 = abs(object1.global_position.y-object2.global_position.y)/(1080-100)
+	var zoom_factor = max(max(zoom_factor1, zoom_factor2), 0.5)
+
+	self.zoom = Vector2(zoom_factor, zoom_factor)
