d33eb3f793cadac8448fd1843e37b9ca79aeead4
HiddenRealm <blacktumble123@gmail.com>
2020-05-15 00:16:03 -0700

    ok camera zoom and follow both players works now

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
-var object1 = NodePath("res://Actors/TestCharacter")
-var object2 = NodePath("res://Actors/TestCharacter")
+export (NodePath) var object1
+export (NodePath) var object2
 
 func _ready():
 	object1 = get_node(object1)
@@ -10,8 +10,8 @@ func _ready():
 func _process(delta):
 	self.global_position = (object1.global_position + object2.global_position) * 0.5
 
-	var zoom_factor1 = abs(object1.global_position.x-object2.global_position.x)/(1920-100)
-	var zoom_factor2 = abs(object1.global_position.y-object2.global_position.y)/(1080-100)
-	var zoom_factor = max(max(zoom_factor1, zoom_factor2), 0.5)
+	var zoom_factor1 = abs(object1.global_position.x-object2.global_position.x)/(600-50)
+	var zoom_factor2 = abs(object1.global_position.y-object2.global_position.y)/(350-50)
+	var zoom_factor = max(max(zoom_factor1, zoom_factor2), 0.9)
 
 	self.zoom = Vector2(zoom_factor, zoom_factor)
